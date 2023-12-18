19418f526c957610295ab3426e85f3b475b8b04a
Turing3 <turing-@naver.com>
2023-12-18 23:31:59 +0900

    C:/Program Files/Git/usr/article/write에 대한 명령어 처리

--- src/main/java/org/sbs/exam/board/Main.java
+++ src/main/java/org/sbs/exam/board/Main.java
@@ -8,11 +8,32 @@ public class Main {
 
     System.out.println("== 게시판 v 0.1 ==");
     System.out.println("== 게시판 시작 ==");
-    System.out.printf("명령)");
-    String cmd = sc.nextLine();
-    System.out.printf("입력된 명령어 : %s\n", cmd);
-    System.out.println("== 게시판 실행 끝 ==");
 
-    sc.close();
+    while (true) {
+      System.out.printf("명령)");
+      String cmd = sc.nextLine();
+
+      if (cmd.equals("/usr/article/write")) {
+        System.out.println("== 계시물 등록 ==");
+        System.out.printf("제목 :");
+        String title = sc.nextLine();
+
+        System.out.printf("내용 :");
+        String body = sc.nextLine();
+
+        int id = 1;
+
+        System.out.printf("%d번 게시물이 등록되었습니다.\n", id);
+      }
+
+        else if (cmd.equals("exit")) {
+        System.out.println("프로그램을 종료합니다.");
+        break;
+      }
+    }
+
+    // System.out.println("== 게시판 실행 끝 ==");
+
+    // sc.close();
   }
 }
